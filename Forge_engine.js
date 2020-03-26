95f61def2237ed687e9c126e27e565aa508f160b
alvaroluna <luna.alvaro3@gmail.com>
2020-03-25 20:52:19 -0700

    Created package.json file

---
+++ Forge_engine.js
@@ -0,0 +1,6 @@
+// npm init to create package.json
+// add api libraries
+// check with "cat package.json" - verify that dependencies are in there
+
+// Create an app in the Forge Developer Portal
+// https://forge.autodesk.com/ - I have an autodesk account
